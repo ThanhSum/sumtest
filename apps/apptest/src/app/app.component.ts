478a6a446474ba43f2d8101a3114c0a4fe7da5b9
Sum Tran <sumtran@hoiio.com>
2023-05-06 22:37:13 +0700

    Initial commit

---
+++ apps/apptest/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sumtest-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'apptest';
+}
